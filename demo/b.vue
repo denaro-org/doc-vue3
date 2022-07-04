af5089a4e3e910d76e5c8bc08bb32803b238b50c
vxhly <pengchengou@gmail.com>
2022-07-04 14:10:39 +0800

    还原错误的更改

--- demo/b.vue
+++ demo/b.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="component-crt-crud">
     嘻嘻嘻嘻嘻嘻嘻
+    <!-- @doc 自定义操作列内容 -->
+    <slot name="customAction"></slot>
+    <!-- @doc 弹窗内容 -->
+    <slot name="modal"></slot>
   </div>
 </template>
 <script lang="ts" setup>
